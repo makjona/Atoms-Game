7faddb06d970a88bf6a915beef1a82e72b66d079
Jonathan Mak <mak.jona@gmail.com>
2015-10-04 17:32:13 -0700

    initial project

---
+++ Test Unity/Assets/BubbleDelete.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BubbleDelete : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (this.transform.position.y > 2){
+            GameObject bubble = (GameObject)Instantiate(Resources.Load("Bubble"));
+            Destroy(this.gameObject);
+        }
+	}
+}
---
+++ Test Unity/Assets/BubbleGravity.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BubbleGravity : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        this.transform.Translate(0, (float) (Time.deltaTime * 2), 0);
+	}
+}
