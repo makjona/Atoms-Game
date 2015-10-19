9acf84a7b59af09b701fcb09df7e8bdfb9ab4b08
Jonathan Mak <mak.jona@gmail.com>
2015-10-18 22:24:17 -0700

    destroy on click, random bubble starting point, animation on destroy

--- Test Unity/Assets/BubbleDelete.cs
+++ Test Unity/Assets/BubbleDelete.cs
@@ -3,16 +3,41 @@ using System.Collections;
 
 public class BubbleDelete : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
+    public const float xRange = (float)(18 - 0.64);
+
+    public Animator anim;
+
+    private bool destroyBubble;
+
+    // Use this for initialization
+    void Start ()
+    {
+        this.gameObject.transform.Translate(Random.value * xRange - xRange / 2, 0, 0);
+        //anim.AddClip(new AnimationClip(), "destroy");
+        anim = GetComponent<Animator>();
+        destroyBubble = false;
+    }
 	
 	// Update is called once per frame
-	void Update () {
-        if (this.transform.position.y > 2){
-            GameObject bubble = (GameObject)Instantiate(Resources.Load("Bubble"));
+	void Update ()
+    {
+        if(this.transform.position.y > 5.34)
+        {
+            Destroy(this.gameObject);
+            UpdateScore.bubblesMissed++;
+        }
+        if(destroyBubble && (anim.GetCurrentAnimatorStateInfo(0).normalizedTime > anim.GetCurrentAnimatorStateInfo(0).length))
+        {
+            print(anim.GetCurrentAnimatorStateInfo(0).normalizedTime + " | " + anim.GetCurrentAnimatorStateInfo(0).length);
             Destroy(this.gameObject);
         }
 	}
+
+    void OnMouseDown()
+    {
+        anim.Play("Destroy", 0);
+        destroyBubble = true;
+        //Destroy(this.gameObject);
+        UpdateScore.bubblesHit++;
+    }
 }
--- Test Unity/Assets/BubbleGravity.cs
+++ Test Unity/Assets/BubbleGravity.cs
@@ -3,15 +3,17 @@ using System.Collections;
 
 public class BubbleGravity : MonoBehaviour {
 
+    public float gravity;
+
 	// Use this for initialization
 	void Start ()
     {
-
+        gravity = Random.value * 4 + 1;
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
-        this.transform.Translate(0, (float) (Time.deltaTime * 2), 0);
+        this.transform.Translate(0, (float) (Time.deltaTime * gravity), 0);
 	}
 }
