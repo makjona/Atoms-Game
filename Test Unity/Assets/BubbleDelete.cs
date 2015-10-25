41b68a8358d9a3e45a874aae783080a4fc156887
Jonathan Mak <mak.jona@gmail.com>
2015-10-24 22:58:20 -0700

    Atoms get random colors, project builds with Andriod sdk, Touch input destroys atoms, idle animation

--- Test Unity/Assets/BubbleDelete.cs
+++ Test Unity/Assets/BubbleDelete.cs
@@ -16,6 +16,11 @@ public class BubbleDelete : MonoBehaviour {
         //anim.AddClip(new AnimationClip(), "destroy");
         anim = GetComponent<Animator>();
         destroyBubble = false;
+
+        Renderer rend = GetComponent<Renderer>();
+        Color randomColor = new Color(Random.value * 20, Random.value * 20, Random.value * 20, 0.1f);
+
+        rend.material.SetColor("_Color", randomColor);
     }
 	
 	// Update is called once per frame
@@ -26,18 +31,28 @@ public class BubbleDelete : MonoBehaviour {
             Destroy(this.gameObject);
             UpdateScore.bubblesMissed++;
         }
-        if(destroyBubble && (anim.GetCurrentAnimatorStateInfo(0).normalizedTime > anim.GetCurrentAnimatorStateInfo(0).length))
+        if(destroyBubble && !anim.GetCurrentAnimatorStateInfo(0).IsName("Idle") && (anim.GetCurrentAnimatorStateInfo(0).normalizedTime > anim.GetCurrentAnimatorStateInfo(0).length))
         {
             print(anim.GetCurrentAnimatorStateInfo(0).normalizedTime + " | " + anim.GetCurrentAnimatorStateInfo(0).length);
             Destroy(this.gameObject);
         }
-	}
+        if(Input.touchCount == 1 && !destroyBubble)
+        {
+            Vector3 wp = Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position);
+            Vector2 touchPos = new Vector2(wp.x, wp.y);
+            if (GetComponent<Collider2D>() == Physics2D.OverlapPoint(touchPos))
+            {
+                anim.Play("Destroy", 0);
+                destroyBubble = true;
+                UpdateScore.bubblesHit++;
+            }
+        }
+    }
 
     void OnMouseDown()
     {
         anim.Play("Destroy", 0);
         destroyBubble = true;
-        //Destroy(this.gameObject);
         UpdateScore.bubblesHit++;
     }
 }
